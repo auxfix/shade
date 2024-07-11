d2db5aa470565c92934563f027648771a4b49165
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-11 22:05:20 +0300

    (canvas): expansion

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: 'A4',
+  pixelsPerInch: 300,
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'orange';
     context.fillRect(0, 0, width, height);
-    context.fillStyle = 'yellow';
-    context.fillRect(width/4, height/4, width/2, height/2);
     context.fillStyle = 'green';
     context.beginPath();
-    context.arc(width/2, height/2, height/4, 0, 2 * Math.PI);
+    context.arc(width/2, height/2, 200, 0, 2 * Math.PI);
     context.fill();
-  };
+    context.lineWidth = 20;
+    context.stroke();
+  }
 };
 
 canvasSketch(sketch, settings);
