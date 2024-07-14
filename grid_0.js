4910d70fcf9f8646ece0d038168ace7815a8a963
oleg shchepotkin <music.wave.form7@gmail.com>
2024-07-14 22:44:34 +0300

    (grid_0): add rundom

--- grid_0.js
+++ grid_0.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: ['200', '200'],
@@ -10,9 +11,9 @@ const margin = 400;
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
-    for( let x = 0; x < 5; x++){
-      for( let y = 0; y < 5; y++){
+    const count = 40;
+    for( let x = 0; x < count; x++){
+      for( let y = 0; y < count; y++){
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
         points.push([ u, v ]);
@@ -21,7 +22,9 @@ const sketch = () => {
 
     return points;
   }
-  const points = createGrid();
+
+  random.setSeed('gtx');
+  const points = createGrid().filter(() => random.value() > 0.5);
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -32,8 +35,8 @@ const sketch = () => {
       const y = lerp(margin, height - margin, v);
       context.strokeStyle = 'black';
       context.beginPath();
-      context.lineWidth = 20;
-      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.lineWidth = 10;
+      context.arc(x, y, 8, 0, Math.PI * 2, false);
       context.stroke();  
     });
   }
